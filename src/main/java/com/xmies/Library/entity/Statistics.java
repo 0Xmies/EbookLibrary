3e6ab575608e3263eacbacda62ecae82345a90f1
Wojciech Mizia <miziawojciech@gmail.com>
2023-09-16 13:57:59 +0200

    update statistics

--- src/main/java/com/xmies/Library/entity/Statistics.java
+++ src/main/java/com/xmies/Library/entity/Statistics.java
@@ -1,8 +1,6 @@
 package com.xmies.Library.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "statistics")
@@ -16,6 +14,11 @@ public class Statistics {
 
     @Column(name = "now_borrowed")
     private int nowBorrowed;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
     public Statistics() {
     }
@@ -57,5 +60,9 @@ public class Statistics {
                 ", timesBought=" + timesBought +
                 ", nowBorrowed=" + nowBorrowed +
                 '}';
+    }
+
+    public int getId() {
+        return id;
     }
 }
