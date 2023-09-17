a7e858f0e630522439e2d1a6994c0e547c4aacf8
Wojciech Mizia <miziawojciech@gmail.com>
2023-09-17 22:44:36 +0200

    fixes
    fix method name in book-repository, add jpa-repository types in statistics

--- src/main/java/com/xmies/Library/repository/StatisticsRepository.java
+++ src/main/java/com/xmies/Library/repository/StatisticsRepository.java
@@ -1,6 +1,7 @@
 package com.xmies.Library.repository;
 
+import com.xmies.Library.entity.Statistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StatisticsRepository extends JpaRepository {
+public interface StatisticsRepository extends JpaRepository<Statistics, Integer> {
 }
