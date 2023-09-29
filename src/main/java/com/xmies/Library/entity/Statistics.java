443db9c80a7a19ddec4ef01a772fb9844de62420
Wojciech Mizia <miziawojciech@gmail.com>
2023-09-29 13:14:02 +0200

    BREAKING CHANGE: new statistics class

--- src/main/java/com/xmies/Library/entity/Statistics.java
+++ src/main/java/com/xmies/Library/entity/Statistics.java
@@ -6,67 +6,80 @@ import jakarta.persistence.*;
 @Table(name = "statistics")
 public class Statistics {
 
-    @Column(name = "times_borrowed")
-    private int timesBorrowed;
+    @Column(name = "menu_entries")
+    private int menuEntries;
 
-    @Column(name = "timesBought")
-    private int timesBought;
+    @Column(name = "book_list_entries")
+    private int bookListEntries;
 
-    @Column(name = "now_borrowed")
-    private int nowBorrowed;
+    @Column(name = "author_list_entries")
+    private int authorListEntries;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
+    @Column(name = "admin_only_requests")
+    private int adminOnlyRequests;
+
+    @Column(name = "publicly_available_requests")
+    private int publiclyAvailableRequests;
 
     public Statistics() {
     }
 
-    public Statistics(int timesBorrowed, int timesBought, int nowBorrowed) {
-        this.timesBorrowed = timesBorrowed;
-        this.timesBought = timesBought;
-        this.nowBorrowed = nowBorrowed;
+    public Statistics(int menuEntries, int bookListEntries, int authorListEntries, int adminOnlyRequests, int publiclyAvailableRequests) {
+        this.menuEntries = menuEntries;
+        this.bookListEntries = bookListEntries;
+        this.authorListEntries = authorListEntries;
+        this.adminOnlyRequests = adminOnlyRequests;
+        this.publiclyAvailableRequests = publiclyAvailableRequests;
     }
 
-    public int getTimesBorrowed() {
-        return timesBorrowed;
+    public int getMenuEntries() {
+        return menuEntries;
     }
 
-    public void setTimesBorrowed(int timesBorrowed) {
-        this.timesBorrowed = timesBorrowed;
+    public void setMenuEntries(int menuEntries) {
+        this.menuEntries = menuEntries;
     }
 
-    public int getTimesBought() {
-        return timesBought;
+    public int getBookListEntries() {
+        return bookListEntries;
     }
 
-    public void setTimesBought(int timesBought) {
-        this.timesBought = timesBought;
+    public void setBookListEntries(int bookListEntries) {
+        this.bookListEntries = bookListEntries;
     }
 
-    public int getNowBorrowed() {
-        return nowBorrowed;
+    public int getAuthorListEntries() {
+        return authorListEntries;
     }
 
-    public void setNowBorrowed(int nowBorrowed) {
-        this.nowBorrowed = nowBorrowed;
+    public void setAuthorListEntries(int authorListEntries) {
+        this.authorListEntries = authorListEntries;
+    }
+
+    public int getAdminOnlyRequests() {
+        return adminOnlyRequests;
+    }
+
+    public void setAdminOnlyRequests(int adminOnlyRequests) {
+        this.adminOnlyRequests = adminOnlyRequests;
+    }
+
+    public int getPubliclyAvailableRequests() {
+        return publiclyAvailableRequests;
+    }
+
+    public void setPubliclyAvailableRequests(int publiclyAvailableRequests) {
+        this.publiclyAvailableRequests = publiclyAvailableRequests;
     }
 
     @Override
     public String toString() {
         return "Statistics{" +
-                "timesBorrowed=" + timesBorrowed +
-                ", timesBought=" + timesBought +
-                ", nowBorrowed=" + nowBorrowed +
+                "menuEntries=" + menuEntries +
+                ", bookListEntries=" + bookListEntries +
+                ", authorListEntries=" + authorListEntries +
+                ", adminOnlyRequests=" + adminOnlyRequests +
+                ", publiclyAvailableRequests=" + publiclyAvailableRequests +
                 '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 }
